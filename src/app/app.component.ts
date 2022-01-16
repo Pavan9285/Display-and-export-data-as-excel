6dda96ecf333147125d7fbb3bc3f39a7c4b7dd6a
pavan.gupta@algorisys.com <pavan.gupta@algorisys.com>
2022-01-16 21:03:03 +0530

    Completed Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'excel';
+
+  constructor() { }
+
+  data = [[1, 2], [3, 4]];
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' }
+  fileName: string = 'SheetJS.xlsx';
+
+  onFileChange(event: any) {
+    const target: DataTransfer = <DataTransfer>(event.target);
+    if (target.files.length !== 1)
+      throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const str = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(str, { type: 'binary' });
+
+      const wsname = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      this.data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+  export() {
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    XLSX.writeFile(wb, this.fileName);
+  }
 }
